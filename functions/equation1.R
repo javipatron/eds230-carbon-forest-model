a79190e0f7c48db7162d07c6769172ae6399c261
JaviPatron <jpatronmf@gmail.com>
2023-05-18 13:43:55 -0700

    some advancement - assignment 6

--- functions/equation1.R
+++ functions/equation1.R
@@ -1,4 +1,12 @@
-# Function for Equation 1: dt/C = r * C
-equation_1 <- function(C, dt, r) {
-  return(dt/C - r*C)
+#' Exponential population growth derivative
+#' 
+#' @param time time since start
+#' @param C forest size measured in units of carbon
+#' @param parms - as list with one value, r
+#' @param r early exponential growth rate
+#' @return derivative of forest size with time
+
+equation1 = function(time, C, parms) {
+  dC = parms$r * C
+  return(list(dC))
 }
--- functions/equation2.R
+++ functions/equation2.R
@@ -1,4 +1,14 @@
-# Function for Equation 2: dC/dt = g * (1 - K/C)
-equation_2 <- function(C, dt, g, K) {
-  return(g * (1 - K/C) - dt)
+#' Forest growth derivative after canopy closure
+#' @param time time since start
+#' @param C forest size measured in units of carbon
+#' @param parms - as list with two values, g, K
+#' @param g linear growth rate of the forest
+#' @param K carrying capacity in units of carbon of the forest
+#' @return derivative of forest size with time
+
+dforestgrowth_closing = function(time, C, parms) {
+  
+  dC = parms$g * (1 - C / parms$K)
+  
+  return(list(dC))
 }
